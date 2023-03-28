aa93f732a72ffef8244aaa2cfbe42d1cb755cadf
Rvjonh <jonhvelasco3@gmail.com>
2023-03-28 19:05:17 -0400

    refact: create task ui

--- src/routes/tasks/index.tsx
+++ src/routes/tasks/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { useNavigate, useParams, Link } from 'react-router-dom';
 
 import { useIsLogged } from "../../hooks/userIsLogged";
 
@@ -55,6 +55,10 @@ export default function TaskRoute() {
         setTaskForm({ ...taskForm, [e.target.name]: e.target.value })
     }
 
+    const handleChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTaskForm({ ...taskForm, [e.target.name]: e.target.value })
+    }
+
     const handleCreation = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
@@ -96,27 +100,57 @@ export default function TaskRoute() {
         return <h2>you need to log in to add tasks</h2>
     }
     return (
-        <div>
-            <h2>Task CREATION</h2>
-            <form onSubmit={handleCreation}>
-                <label htmlFor="title">
-                    Title:
-                    <input type="text" name="title" value={taskForm.title} onChange={handleChange} placeholder="title" required />
-                </label>
-                <label htmlFor="description">
-                    Description:
-                    <input type="text" name="description" value={taskForm.description} onChange={handleChange} placeholder="description" required />
-                </label>
-                <button type="submit">
-                    {taskId != undefined ?
-                        "Update"
-                        :
-                        "Create"
-                    }
-                    Task
-                </button>
-            </form>
-            <ServerMessages messages={serverMessages} />
+        <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+            <div className="w-full max-w-md space-y-8  p-4 pb-10 square-back">
+
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    Task Creation
+                </h2>
+
+                <ServerMessages messages={serverMessages} />
+
+                <form className="flex flex-col" onSubmit={handleCreation}>
+
+                    <label className="flex flex-col" htmlFor="title">
+                        Title:
+                        <input type="text"
+                            name="title"
+                            value={taskForm.title}
+                            onChange={handleChange}
+                            placeholder="Title"
+                            required
+                            maxLength={100}
+                            className="w-full border-2 rounded border-blue-500 p-2"
+                        />
+                    </label>
+
+                    <label className="" htmlFor="description">
+                        Description:
+                        <textarea
+                            name="description"
+                            id="description"
+                            value={taskForm.description}
+                            onChange={handleChangeText}
+                            rows={10}
+                            placeholder="an awesome description"
+                            required
+                            maxLength={255}
+                            className="w-full border-2 rounded border-blue-500 p-2"
+                        >
+                        </textarea>
+                    </label>
+
+                    <div className="flex items-center justify-between">
+                        <Link to='/' className="group relative flex justify-center rounded-md  py-2 px-3 text-sm font-semibold text-white  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 bg-red-600 hover:bg-red-500">
+                            Cancel
+                        </Link>
+                        <button type="submit" className="group relative flex justify-center rounded-md bg-green-600 py-2 px-3 text-sm font-semibold text-white hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+                            {taskId != undefined ? "Update " : "Create "}Task
+                        </button>
+                    </div>
+
+                </form>
+            </div>
         </div>
     )
 }

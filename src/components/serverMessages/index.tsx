b5a623d979913156e5a991f2e8ee93b193d32637
Rvjonh <jonhvelasco3@gmail.com>
2023-03-28 17:02:52 -0400

    refact: password-reset ui

--- src/components/serverMessages/index.tsx
+++ src/components/serverMessages/index.tsx
@@ -10,18 +10,23 @@ export function ServerMessages(props: ComponentProps) {
 
     const returnListItemOrString = (myArray: string[] | string) => {
         if (typeof myArray === 'string') {
-            return myArray
+            return <li className='capitalize'>{myArray}</li>
         }
         return myArray.map((item) => (
-            <li key={item}>{item}</li>
+            <li key={item} className='capitalize'>
+                {item}
+            </li>
         ))
+    }
+    if (!messages) {
+        return <i></i>
     }
 
     return (
-        <div>
+        <div className='border-2 border-red p-2 bg-amber-100'>
             {messages && Object.entries(messages).map(([key, value]) => (
                 <div key={key}>
-                    <h2>{key}</h2>
+                    <h3 className='capitalize text-lg'>{key}</h3>
                     <ul>
                         {returnListItemOrString(value)}
                     </ul>
--- src/routes/passwordReset/index.tsx
+++ src/routes/passwordReset/index.tsx
@@ -3,12 +3,15 @@ import React, { useState } from "react"
 import { Errors } from "../../interfaces/errorsRequest"
 
 import { ServerMessages } from './../../components/serverMessages'
+import { Spinner } from "../../components/spinner"
 
 import TasksBackend from './../../services/tasksBackend'
 
 export default function PasswordResetRoute() {
     const [email, setEmail] = useState("")
     const [messages, setMessages] = useState<Errors>()
+    const [loading, setLoading] = useState<boolean>(false)
+
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setEmail(e.target.value)
@@ -16,28 +19,61 @@ export default function PasswordResetRoute() {
 
     const handleSubmitPasswordReset = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        setLoading(true)
         TasksBackend.resetPassword(email).then(res => {
             if ('detail' in res) {
                 setMessages({ 'success': [res['detail']] })
             } else {
                 setMessages(res)
             }
+            setLoading(false)
         })
     }
 
     return (
-        <div>
-            <h1>Password Reset</h1>
-            <p>We will send you an email with a code to change your password</p>
-            <p>If you don't recive anything maybe you need to create an account first</p>
-            <form onSubmit={handleSubmitPasswordReset}>
-                <label htmlFor="resetEmail">
-                    Email:
-                    <input type="email" name="resetEmail" value={email} onChange={handleChange} placeholder="youremail@email.com" />
-                </label>
-                <button type="submit">Send Email</button>
-            </form>
-            <ServerMessages messages={messages} />
+        <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+            <div className='w-full max-w-md space-y-8'>
+
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Password Reset</h2>
+                <p className="text-justify">We will send you an email with a code to change your password</p>
+                <p className="text-justify">If you don't recive anything maybe you need to create an account first</p>
+                <ServerMessages messages={messages} />
+                <form className="mt-8 space-y-6" onSubmit={handleSubmitPasswordReset}>
+                    <div className="-space-y-px rounded-md shadow-sm">
+                        <div>
+                            <label htmlFor="resetEmail" className="sr-only">
+                                Email:
+                            </label>
+                            <input type="email"
+                                id="email-address"
+                                name="resetEmail"
+                                autoComplete="email"
+                                value={email}
+                                onChange={handleChange}
+                                required
+                                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-1"
+                                placeholder="Email address"
+                                disabled={loading}
+                            />
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            type="submit"
+                            className="group relative flex w-full flex-col justify-center
+                                items-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            disabled={loading}
+                        >
+                            {loading ?
+                                <Spinner size='.5em' color="#ffffff" />
+                                :
+                                "Login"
+                            }
+                        </button>
+                    </div>
+                </form>
+            </div>
+
         </div>
     )
 }

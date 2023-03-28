0d9b758be408419cfb298167e2b46e5853363e34
Rvjonh <jonhvelasco3@gmail.com>
2023-03-28 17:35:53 -0400

    refact: password-reset-confirmation ui

--- src/routes/passwordResetConfirmation/index.tsx
+++ src/routes/passwordResetConfirmation/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 import { MyResetPasswordConfirmationForm } from "../../interfaces/passwordResetConfirmation";
 import { Errors } from "../../interfaces/errorsRequest";
 
 import { ServerMessages } from "../../components/serverMessages";
+import { Spinner } from "../../components/spinner";
 
 import TasksBackend from './../../services/tasksBackend'
 
@@ -18,6 +19,7 @@ export default function PasswordResetConfirmationRoute() {
         "token": token ?? ""
     })
     const [messages, setMessages] = useState<Errors>()
+    const [loading, setLoading] = useState<boolean>(false)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setUserForm({ ...userForm, [e.target.name]: e.target.value })
@@ -25,6 +27,7 @@ export default function PasswordResetConfirmationRoute() {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        setLoading(true)
         TasksBackend.resetPasswordConfirm(userForm)
             .then(res => {
                 if ('detail' in res) {
@@ -32,24 +35,98 @@ export default function PasswordResetConfirmationRoute() {
                 } else {
                     setMessages(res)
                 }
+                setLoading(false)
             })
+    }
+    const showLoginButton = () => {
+        if (messages) {
+            const keys_msjs = Object.keys(messages);
+            return keys_msjs.includes('detail') || keys_msjs.includes('token');
+        }
+        return false;
     }
 
     return (
-        <div>
-            <h2>Password change</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="new_password1">
-                    new password:
-                    <input type="password" name="new_password1" value={userForm.new_password1} onChange={handleChange} placeholder="your new password" />
-                </label>
-                <label htmlFor="new_password2">
-                    new password:
-                    <input type="password" name="new_password2" value={userForm.new_password2} onChange={handleChange} placeholder="your new password again" />
-                </label>
-                <button type="submit">Change Password</button>
-            </form>
-            <ServerMessages messages={messages} />
+        <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+            <div className='w-full max-w-md space-y-8 p-4 pb-10 square-back'>
+
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Password change</h2>
+
+                <p className="text-justify">Welcome, you can change your password here:</p>
+
+                <ServerMessages messages={messages} />
+
+                <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+
+                    <div>
+                        <label htmlFor="new_password1" className="sr-only">
+                            Password:
+                        </label>
+                        <input
+                            id="password1"
+                            type="password"
+                            name="new_password1"
+                            value={userForm.new_password1}
+                            onChange={handleChange}
+                            autoComplete="current-password"
+                            required
+                            className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-1"
+                            placeholder="New Password 1"
+                            disabled={showLoginButton()}
+
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="new_password2" className="sr-only">
+                            Password:
+                        </label>
+                        <input
+                            id="password2"
+                            type="password"
+                            name="new_password2"
+                            value={userForm.new_password2}
+                            onChange={handleChange}
+                            autoComplete="current-password"
+                            required
+                            className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-1"
+                            placeholder="New Password 2"
+                            disabled={showLoginButton()}
+                        />
+                    </div>
+                    {showLoginButton() &&
+                        <div className="flex items-center justify-end">
+                            <div className="text-sm">
+                                <Link to='/password-reset' className="font-medium text-indigo-600 hover:text-indigo-500">
+                                    Ask new token for your password?
+                                </Link>
+                            </div>
+                        </div>
+                    }
+                    <div>
+                        {!showLoginButton() ?
+                            <button
+                                type="submit"
+                                className="group relative flex w-full flex-col justify-center
+                                items-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                disabled={loading}
+                            >
+                                {loading ?
+                                    <Spinner size='.5em' color="#ffffff" />
+                                    :
+                                    "Change Password"
+                                }
+                            </button>
+                            :
+                            <Link to='/login'
+                                className="group relative flex w-full flex-col justify-center
+                            items-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                Log in
+                            </Link>
+                        }
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }

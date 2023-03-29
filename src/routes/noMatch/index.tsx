8ed532c2479ee2c3f47375a110046499da413a7c
Rvjonh <jonhvelasco3@gmail.com>
2023-03-28 23:23:39 -0400

    feat: page error404 ui

--- src/routes/noMatch/index.tsx
+++ src/routes/noMatch/index.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
 
+import './style.css';
+
+import backImage from './assets/error.png';
 
 export default function NoMatch() {
-    return <h1>NoMatch</h1>
+
+    useEffect(() => {
+        document.title = "Page No Found";
+    }, []);
+
+    return (
+        <main className="main-noPageFound">
+            <div className="back-img-noPageFound">
+                <img src={backImage} alt="no se ha encontrado la pagina imagen" />
+            </div>
+            <h2 className="title-noPageFound">ERROR 404</h2>
+            <h3 className="subtitle-noPageFound">Page no found</h3>
+        </main>
+    );
 }

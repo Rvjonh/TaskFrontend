b11fec2560b9813ba5bda9a53e62b6e3742cec7d
Rvjonh <jonhvelasco3@gmail.com>
2023-03-29 12:03:40 -0400

    feat: hide task urls when user is not logged

--- src/routes/ProviderRouter.tsx
+++ src/routes/ProviderRouter.tsx
@@ -15,24 +15,34 @@ import TaskHistoryRoute from './taskHistory'
 
 import NoMatch from './noMatch'
 
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
 export default function ProviderRouter() {
+  const { identificador } = useSelector((state: RootState) => state.login);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomeRoute />} />
 
+
           <Route path="login" element={<LoginRoute />} />
           <Route path="signup" element={<SignUpRoute />} />
-          <Route path="password-reset" element={<PasswordResetRoute />} />
           <Route path="password-change" element={<PasswordChangeRoute />} />
+
+          <Route path="password-reset" element={<PasswordResetRoute />} />
           <Route path="api/v1/password-reset/confirm/:id/:token/" element={<PasswordResetConfirmationRoute />} />
 
-          <Route path="task" element={<TaskRoute />} />
-          <Route path="task/:id/" element={<TaskDetailRoute />} />
-          <Route path="task/:id/update/" element={<TaskRoute />} />
+          {/* available when user is logged */}
+          {identificador && <>
+            <Route path="task" element={<TaskRoute />} />
+            <Route path="task/:id/" element={<TaskDetailRoute />} />
+            <Route path="task/:id/update/" element={<TaskRoute />} />
 
-          <Route path="task/history/" element={<TaskHistoryRoute />} />
+            <Route path="task/history/" element={<TaskHistoryRoute />} />
+          </>}
+
 
           <Route path="*" element={<NoMatch />} />
         </Route>

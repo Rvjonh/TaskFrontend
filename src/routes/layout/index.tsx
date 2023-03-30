2fcdd8008128511af8dfdbcbf2cb0a37886cd570
Rvjonh <jonhvelasco3@gmail.com>
2023-03-29 23:38:19 -0400

    update title when

--- src/routes/layout/index.tsx
+++ src/routes/layout/index.tsx
@@ -1,4 +1,5 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom'
 
 import NavbarLink from './../../components/navbar'
 import { TaskNavbar } from '../../components/tasksNavbar';
@@ -8,7 +9,14 @@ import { useIsLogged } from '../../hooks/userIsLogged';
 import AnimatedFooter from '../../components/animatedFooter';
 
 export function Layout() {
-  const userActive = useIsLogged()
+  const location = useLocation();
+  const userActive = useIsLogged();
+
+  useEffect(() => {
+    if (document.title === "Page No Found") {
+      document.title = "Task App";
+    }
+  }, [location]);
 
   return (
     <div className='min-h-screen flex flex-col relative'>
